9b20309cb43f084cf168c6bc7f3ee2f0f0768af4
geronMota <geron_22@hotmail.com>
2026-03-15 15:19:38 -0300

    Fix: correct cucumber feature path

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/features",
+        features = "src/test/resources/features",
         glue = {"stepdefinitions", "hook"},
         plugin = {"pretty","html:target/cucumber/cucumber.html",
                             "json:target/cucumber/cucumber.json",
