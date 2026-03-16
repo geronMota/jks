1c7c87bc6a02077f6a29829dfafc280332be3a7f
geronMota <geron_22@hotmail.com>
2026-03-15 22:23:59 -0300

    adding report to the branch

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hook"},
-        plugin = {"pretty"}
+        plugin = {"pretty","html:target/cucumber/cucumber.html",
+                            "json:target/cucumber/cucumber.json"}
 
 
 
